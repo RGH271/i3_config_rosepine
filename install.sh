7575e7a08bc04a0bcba0e0395f67422e53ed3189
RGH271 <rhorrocks1@outlook.com>
2024-04-13 21:02:26 +0100

    fixed some things

--- install.sh
+++ install.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 # please work
 
+# install dependencies
+sudo pacman -S feh picom materia-gtk-theme papirus-icon-theme lxappearance ttf-font-awesome ttf-ubuntu-font-family ttf-droid breeze-icons
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
+cd ~
+yay -S vesktop-bin
+yay -S spotify
+git clone https://git.suckless.org/st && cd st && sudo make clean install
+cd ~
+sudo rm -rf st && sudo rm -rf yay-bin
+
+
+# now clone the configs
 git clone https://github.com/RGH271/i3_config.git
 mkdir ~/.config/wallpaper && sudo cp ~/i3_config/wallpaper/wallpaper.jpg ~/.config/wallpaper/
 sudo cp -a ~/i3_config/scripts/. ~/.config/scripts/ && sudo chmod +x ~/.config/scripts/*
