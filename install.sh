1897277a2f308492c1e19c3c81803de3837dc86f
RGH271 <rhorrocks1@outlook.com>
2024-05-04 15:45:54 +0100

    update install script

--- install.sh
+++ install.sh
@@ -2,49 +2,48 @@
 
 
 # install graphical softwares
-printf "Installing graphics software..."
+printf "Installing graphics software...\n"
 sudo pacman -S --noconfirm polybar feh picom lxappearance rofi
 
 # install fonts
-printf "\nInstalling fonts..."
+printf "\nInstalling fonts...\n"
 sudo pacman -S --noconfirm ttf-font-awesome ttf-ubuntu-font-family ttf-droid ttf-firacode-nerd
 
 # install other dependencies
-printf "\nInstalling other dependencies..."
+printf "\nInstalling other dependencies...\n"
 sudo pacman -S --noconfirm breeze-icons dolphin python-dbus python-gobject pacman-contrib unzip
 
 # INstall the cursor
-printf "\nInstalling the cursor..."
+printf "\nInstalling the cursor...\n"
 git clone https://github.com/SueDonham/Colloid-pastel-icons.git && cd ~/Colloid-pastel-icons/cursors/ && ./install.sh && cd 
 
 # install rust/cargo
-printf "\nInstalling rust..."
+printf "\nInstalling rust...\n"
 curl https://sh.rustup.rs -sSf | sh
 
-printf "Do you want to install YAY? (y/N)"
-read -r choice < /dev/tty
-if [ "$choice" = "Y" ] || [ "$choice" = "y" ]; then
-    printf "\nInstalling YAY..."
-    sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si && cd ~
-fi
+# install yay
+printf "\nInstalling YAY...\n"
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si && cd ~
 
 printf "Do you want to install vesktop? (y/N)"
 read -r choice < /dev/tty
 if [ "$choice" = "Y" ] || [ "$choice" = "y" ]; then
-    printf "\nInstalling vesktop..."
+    printf "\nInstalling vesktop...\n"
     yay -S vesktop-bin
+    continue
 fi
 
 printf "Do you want to install spotify? (y/N)"
 read -r choice < /dev/tty
 if [ "$choice" = "Y" ] || [ "$choice" = "y" ]; then
-    printf "\nInstalling spotify and spotify_player..."
+    printf "\nInstalling spotify and spotify_player...\n"
     yay -S spotify-player
     cargo install spotify_player
+    continue
 fi
 
 # install simple terminal
-printf "\nInstalling the terminal..."
+printf "\nInstalling the terminal...\n"
 git clone https://github.com/RGH271/st.git && cd st && sudo make clean install && cd
 
 # remove the repos
@@ -59,7 +58,8 @@ git clone https://github.com/RGH271/i3_config_rosepine.git
 mkdir ~/.config/wallpaper && sudo cp ~/i3_config_rosepine/wallpaper/wallpaper.png ~/.config/wallpaper/
 
 # copy polybar configs
-mkdir ~/.config/polybar && sudo cp -r ~/i3_config_rosepine/polybar/* ~/.config/polybar/ && sudo chmod +x ~/.config/polybar/scripts/updates-pacman-helper.sh && sudo chmod +x ~/.config/polybar/launch.sh
+mkdir ~/.config/polybar && sudo cp -r ~/i3_config_rosepine/polybar/* ~/.config/polybar/ 
+sudo chmod +x ~/.config/polybar/scripts/updates-pacman-aurhelper.sh && sudo chmod +x ~/.config/polybar/launch.sh
 
 # set the picom
 mkdir ~/.config/picom && sudo cp ~/i3_config_rosepine/picom/picom.conf ~/.config/picom/
