e406fd275fa5bd97ff0e57c3d2fe238c15af4876
RGH271 <rhorrocks1@outlook.com>
2024-05-07 19:16:31 +0100

    fixes

--- install.sh
+++ install.sh
@@ -29,7 +29,9 @@ sudo rm -rf st
 
 # install yay
 printf "\nInstalling YAY...\n"
-sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si && cd 
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si 
+Y
+cd 
 sudo rm -rf yay-bin
 
 : '
--- yay.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-
-# install yay
-printf "\nInstalling YAY...\n"
-sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si 
-Y
-cd 
-sudo rm -rf yay-bin
