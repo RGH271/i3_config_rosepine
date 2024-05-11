312900afce999b92c1088e9e3b5ad2bd68d086d2
RGH271 <rhorrocks1@outlook.com>
2024-05-11 13:02:32 +0100

    reformated the files and edited the two config scripts

--- config.sh
+++ config.sh
@@ -5,15 +5,10 @@
 # clone the repo
 git clone https://github.com/RGH271/i3_config_rosepine.git
 
-# set the wallpaper
-mkdir ~/.config/wallpaper && sudo cp ~/i3_config_rosepine/wallpaper/wallpaper.png ~/.config/wallpaper/
+sudo cp configfiles/* ~/.config/
 
-# copy polybar configs
-mkdir ~/.config/polybar && sudo cp -r ~/i3_config_rosepine/polybar/* ~/.config/polybar/ 
+# set the polybar scripts to executable
 sudo chmod +x ~/.config/polybar/scripts/updates-pacman-aurhelper.sh && sudo chmod +x ~/.config/polybar/launch.sh
-
-# set the picom
-mkdir ~/.config/picom && sudo cp ~/i3_config_rosepine/picom/picom.conf ~/.config/picom/
 
 # set master i3 config
 cp ~/i3_config_rosepine/main_conf_files/config ~/.config/i3/ && sudo chmod a+rw ~/.config/i3/config
--- resave.sh
+++ resave.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 
 # make sure the repo is up to date
-cd ~/i3_config_rosepine && git pull && cd
+cd ~/i3_config_rosepine && git pull 
+cd configfiles
 
 # recopy the wallpaper
-sudo rm -r ~/i3_config_rosepine/wallpaper/* && sudo cp ~/.config/wallpaper/* ~/i3_config_rosepine/wallpaper/
+sudo rm -r wallpaper/* && sudo cp ~/.config/wallpaper/* wallpaper/
 # save the picom configs
-sudo rm -r ~/i3_config_rosepine/picom/* && sudo cp ~/.config/picom/picom.conf ~/i3_config_rosepine/picom/
+sudo rm -r picom/* && sudo cp ~/.config/picom/picom.conf picom/
 # save polybar configs and scripts
-sudo rm -r ~/i3_config_rosepine/polybar/* && sudo cp -r ~/.config/polybar/* ~/i3_config_rosepine/polybar/
+sudo rm -r polybar/* && sudo cp -r ~/.config/polybar/* polybar/
+# save the rofi config
+sudo rm -rf rofi/* 
+sudo cp -r ~/.config/rofi/* rofi/ 
+# save the nvim configs
+sudo rm -rf nvim/* 
+sudo cp -r ~/.config/nvim/* nvim/
+# save x11 files
+sudo rm -rf X11/*
+sudo cp -r ~/.config/X11/* X11/
 
 # remove and copy all the important config files
 sudo rm -r ~/i3_config_rosepine/main_conf_files/*
